67497856afbc5ec2da842ae3de37658a41fa8eed
BeastNeekit <99237918+BeastNeekit@users.noreply.github.com>
2024-07-04 10:43:43 +0545

    Update Urls.js

--- Frontend/src/Urls.js
+++ Frontend/src/Urls.js
@@ -1,2 +1,2 @@
-export const baseUrl = `https://testing-frontend-2pq8.vercel.app`
-export const backUrl = `https://testing-frontend-2pq8.vercel.app`
+export const baseUrl = `https://testing-frontend-lac.vercel.app`
+export const backUrl = `https://testing-frontend-lac.vercel.app`
