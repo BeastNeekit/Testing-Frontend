041657b9eb3fe0629359c76a233d091d2b4ec2dc
Neekit <99237918+BeastNeekit@users.noreply.github.com>
2024-07-29 14:33:39 +0545

    Update Product.js

--- Backend/model/Product.js
+++ Backend/model/Product.js
@@ -1,11 +1,27 @@
-// models/Product.js
-
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String },
     price: { type: Number, required: true },
+    updateTime: { type: Date, required: true }, // New field for update time
+    category: {
+        type: String,
+        required: true,
+        enum: [
+            'cigarette',
+            'noodle',
+            'beans',
+            'grain',
+            'soap',
+            'electronic',
+            'drink',
+            'sanity',
+            'chocolate',
+            'biscuit',
+            'others'
+        ] // New field for category with predefined values
+    },
     createdAt: { type: Date, default: Date.now },
 });
 
