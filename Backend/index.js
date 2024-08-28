c3bd44b125407da87bf369e2e9e2b22274140fa1
Neekit <99237918+BeastNeekit@users.noreply.github.com>
2024-08-28 17:40:23 +0545

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,38 +6,41 @@ const cors = require('cors');
 const userRoutes = require('./Routes/user');
 const productRoutes = require('./Routes/productRoutes');
 const adminRouter = require('./Routes/admin'); 
-require("dotenv").config();
+
+require("dotenv").config
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT ;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: ["https://testing-frontend-2pq8.vercel.app"],
-    methods: ["POST", "GET", "PUT"],
-    credentials: true
-}));
-
-app.use("/oder", orderRoutes);
+app.use(cors(
+    {
+      origin: ["https://testing-frontend-2pq8.vercel.app"],
+      methods: ["POST", "GET"],
+      credentials: true
+    }
+));
+app.use("/order", orderRoutes);
 app.use('/user', userRoutes);
 app.use('/products', productRoutes);
-app.use('/admin', adminRouter); 
+app.use('/admin', adminRouter);
 
 app.get("/", async (req, res) => {
-    try {
-        res.status(200).json({ msg: "I am in home route" });
-    } catch (error) {
-        res.status(500).json({ msg: "Error in home route" });
-    }
+  try {
+    res.status(200).json({ msg: "I am in home route" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error in home route" });
+  }
 });
 
+
 app.listen(port, async () => {
-    try {
-        await connection(); 
-        console.log(colors.bgYellow(`Connected to MongoDB`));
-    } catch (error) {
-        console.log(colors.bgRed("Error connecting to MongoDB"));
-    }
-    console.log(colors.rainbow(`Backend is running on port ${port}`));
+  try {
+    await connection;
+    console.log(colors.bgYellow(`connectd to mongo db`));
+  } catch (error) {
+    console.log(colors.bgRed("Error in connecting mongoDb"));
+  }
+  console.log(colors.rainbow(`Backend is running on port ${port}`));
 });
