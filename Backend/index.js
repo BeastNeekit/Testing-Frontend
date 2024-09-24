c2e81e35336a1a33c13e2d9ca02ba01adcc2bceb
Neekit <99237918+BeastNeekit@users.noreply.github.com>
2024-09-24 11:13:28 +0545

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-      origin: ["https://shopefi.vercel.app"],
+      origin: ["https://testing-frontend-2pq8.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
     }
